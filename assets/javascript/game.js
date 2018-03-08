8d7889888bfc3cfc3984f714b8e9a9d9180c14f9
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-08 00:17:54 -0500

    Change crystal placeholders to buttons, add beginning of logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,47 @@
 var currentScore;
 var targetScore;
 
-var crystal1Value;
-var crystal2Value;
-var crystal3Value;
-var crystal4Value;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
 
 var crystalValues = [];
 
 // $('.crystal')
+
+// jQuery objects
+
+
 
 // inclusive
 function randomInt(min, max) {
     return Math.floor(min + Math.random() * (1 + max - min));
 }
 
+
+function updateGameBoard() {
+    
+}
+
+
 $( document ).ready(function() {
 
 
     console.log( "ready!" );
     // rest of jQuery action goes here
+
+    var crystal1 = $('#crystal-1');
+    var crystal2 = $('#crystal-2');
+    var crystal3 = $('#crystal-3');
+    var crystal4 = $('#crystal-4');
+    
+
+
+
     $('.crystal').click(function() {
         // this is going to not randomize upon click, of course
-        console.log('crystal score: ' + randomInt(1, 12));
+        console.log('crystal score: ' + $(this).val());
     });
         
     $('#target-score').click(function() {
@@ -32,7 +51,23 @@ $( document ).ready(function() {
     });
 
 
+    function resetGame() {
+        crystal1.val(randomInt(1, 12));
+        crystal2.val(randomInt(1, 12));
+        crystal3.val(randomInt(1, 12));
+        crystal4.val(randomInt(1, 12));
+        // crystal1Value = randomInt(1, 12);
+        // crystal2Value = randomInt(1, 12);
+        // crystal3Value = randomInt(1, 12);
+        // crystal4Value = randomInt(1, 12);
+        targetScore = randomInt(19, 120);
+    }
+    
 
+    $('#start-game').click(function() {
+        resetGame();
+
+    });
 
 
 
