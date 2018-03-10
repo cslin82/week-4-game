470ebf3cac244433c28d93b9529a1ce07e7c38ff
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-09 19:06:15 -0500

    Add jQuery click handlers

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -9,17 +9,32 @@ function randomInt(min, max) {
     return Math.floor(min + Math.random() * (1 + max - min));
 }
 
-// Fill crystalValues with random from 1 to 12
 crystalValues.length = 4;
 // crystalValues.fill(0);
-// could probably use something fancier like map or foreach
-for (let i = 0; i < crystalValues.length; i++) {
-    crystalValues[i] = randomInt(1, 12);    
-}
 
+function resetGame() {
+    // Fill crystalValues with random from 1 to 12
+    // could probably use something fancier like map or foreach
+    for (let i = 0; i < crystalValues.length; i++) {
+        crystalValues[i] = randomInt(1, 12);    
+    }
+}
+    
 // jQuery main
 $(document).ready(function() {
 
     console.log( "ready!" );
 
+    $('#target-score').click(function() {
+        $(this).text(randomInt(19, 120));   
+    });
+
+    $('#start-game').click(function() {
+        resetGame();
+    });
+
+    $('.crystal').click(function() {
+        // console.log('crystal score this-attr-value: ' + $(this).attr("value"));
+        console.log('crystal score array: ' + crystalValues[$(this).attr("array-index")]);
+    });
 });
